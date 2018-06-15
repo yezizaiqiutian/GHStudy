eb3139f75c7baea1c6bb2c3d256114737a936c17
gh <15652919252@163.com>
2018-06-15 10:17:25 +0800

    初始提交

---
+++ app/src/main/java/com/gh/ghstudy/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.gh.ghstudy;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author: gh
+ * @description:
+ * @date: 2018/6/15.
+ * @from:
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+    }
+}
